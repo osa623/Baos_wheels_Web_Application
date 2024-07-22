dbbf1cbf654ae5ae7e1412d924534aeb14681149
osa623 <it22170798@my.sliit.lk>
2024-07-23 00:20:34 +0530

    last git push for today

--- frontend/src/Pages/Articlesec.js
+++ frontend/src/Pages/Articlesec.js
@@ -17,18 +17,56 @@ useEffect(()=>{
   return (
     <div className='relative h-auto w-auto'>
 
-      <div className='flex flex-col w-full h-auto bg-primary items-center justify-start'>
-        <div className='flex md:h-[10vh] sms:h-[5vh]   w-full items-center justify-center' />
-        
-        <div className='flex flex-col h-[20vh] w-full items-center justify-center'>
-          <div className='flex md:w-[30vw] sms:w-[50vw] h-0.5 bg-secondary' data-aos='fade-right' />
-          <h2 className='flex text-6xl sms:text-7xl font-russoone text-baseextra4' data-aos='flip-up'>Articles</h2>
-          <div className='flex md:w-[30vw] sms:w-[50vw] h-0.5 bg-secondary' data-aos='fade-left' />
-        </div>
+      <div className='flex flex-col w-full h-auto bg-primary items-center justify-start sms:pt-20'>
 
-        <div className='flex flex-col h-auto w-full items-center justify-center'>
-            <p className='flex h-autp w-auto '></p>
-        </div>
+        
+              <div className='flex flex-col sms:h-[15vh] w-full items-center justify-center'>
+                    <div className='flex md:w-[30vw] sms:w-[50vw] h-0.5 bg-secondary' data-aos='fade-right' />
+                    <h2 className='flex text-6xl sms:text-7xl font-russoone text-baseextra4' data-aos='flip-up'>Articles</h2>
+                    <div className='flex md:w-[30vw] sms:w-[50vw] h-0.5 bg-secondary' data-aos='fade-left' />
+              </div>
+
+              <div className='flex flex-col h-auto w-full items-center justify-center p-5'>
+                    <p className='flex h-auto w-full text-center font-kanit'>Our Articles section is your go-to destination for all things automotive. From insights into mechanical parts and the latest car inventions to coverage of auto companies and industry news, we have it all. I also share my thoughts on the most groundbreaking advancements in the automotive world. Whether you're an enthusiast or curious, this section offers a wealth of knowledge and captivating content.</p>
+              </div>
+
+              <div className='sm:hidden flex h-auto w-full items-center justify-between p-2'>
+                    
+                      <form class="max-w-lg mx-auto">
+                          <div class="flex">
+                              <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
+                              <button id="dropdown-button" data-dropdown-toggle="dropdown" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">All categories <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                        </svg></button>
+                              <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                                  <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
+                                  <li>
+                                      <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mockups</button>
+                                  </li>
+                                  <li>
+                                      <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Templates</button>
+                                  </li>
+                                  <li>
+                                      <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Design</button>
+                                  </li>
+                                  <li>
+                                      <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logos</button>
+                                  </li>
+                                  </ul>
+                              </div>
+                              <div class="relative w-full">
+                                  <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
+                                  <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                      <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                      </svg>
+                                      <span class="sr-only">Search</span>
+                                  </button>
+                              </div>
+                          </div>
+                      </form>
+
+              </div>
         </div>
         </div>
   )
