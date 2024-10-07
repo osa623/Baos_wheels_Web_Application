63f308dc8b54c1627324b64451c1fea6e7ba15d1
osa623 <it22170798@my.sliit.lk>
2024-10-07 19:14:18 +0530

    push

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';  // Import Link from react-router-dom
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import bwlogo from '../assests/baoswheelslogo.png';
 
@@ -15,10 +16,19 @@ const Navbar = () => {
         <img src={bwlogo} alt="bwlogo" className="h-12 w-auto m-4 mds:h-8 mds:m-2 lgs:h-12 sms:h-8" />
       </div>
       <ul className="hidden md:flex items-center font-russoone text-primary text-xl cursor-pointer">
-        <li className="p-4 hover:text-baseprimary focus:text-baseprimary"><a href='/'>Home</a></li>
-        <li className="p-4 hover:text-baseprimary focus:text-baseprimary"><a href='/reviews'>Reviews</a></li>
-        <li className="p-4 hover:text-baseprimary"><a href='/articles'>Articles</a></li>
-        <li className="p-4 hover:text-baseprimary"><a href='/about'>About</a></li>
+        {/* Use Link component for routing */}
+        <li className="p-4 hover:text-baseprimary focus:text-baseprimary">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="p-4 hover:text-baseprimary focus:text-baseprimary">
+          <Link to="/reviews">Reviews</Link>
+        </li>
+        <li className="p-4 hover:text-baseprimary">
+          <Link to="/articles">Articles</Link>
+        </li>
+        <li className="p-4 hover:text-baseprimary">
+          <Link to="/about">About</Link>
+        </li>
       </ul>
       <div className="md:hidden" onClick={handleNav}>
         {nav ? <AiOutlineClose size={30} color="#FFFFFF" /> : <AiOutlineMenu size={30} color="#FFFFFF" />}
@@ -30,10 +40,19 @@ const Navbar = () => {
           <div className="flex items-center justify-center">
             <img src={bwlogo} alt="bwlogo" className="h-8 w-auto m-4" />
           </div>
-          <li className="p-4 border-b border-gray-600 text-center"><a href='/'>Home</a></li>
-          <li className="p-4 border-b border-gray-600 text-center"><a href='/reviews'>Reviews</a></li>
-          <li className="p-4 border-b border-gray-600 text-center"><a href='/articles'>Articles</a></li>
-          <li className="p-4 border-b border-gray-600 text-center"><a href='/about'>About</a></li>
+          {/* Use Link component for mobile navigation */}
+          <li className="p-4 border-b border-gray-600 text-center">
+            <Link to="/" onClick={handleNav}>Home</Link>
+          </li>
+          <li className="p-4 border-b border-gray-600 text-center">
+            <Link to="/reviews" onClick={handleNav}>Reviews</Link>
+          </li>
+          <li className="p-4 border-b border-gray-600 text-center">
+            <Link to="/articles" onClick={handleNav}>Articles</Link>
+          </li>
+          <li className="p-4 border-b border-gray-600 text-center">
+            <Link to="/about" onClick={handleNav}>About</Link>
+          </li>
         </ul>
         <div className='flex items-center justify-center p-4 gap-4'>
           <span className="text-gray-400 hover:text-primary transition-transform cursor-pointer">
